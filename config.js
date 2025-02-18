c01e78bd00e03f98ccbf16dca46873435147681b
Isitha anuss <isithaanusara01@gmail.com>
2025-02-18 18:50:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "D7JDSKoJ#AATmEygoowdid_P-bQ_o_tp6Q2NxhudiOyind1nfl3g",
+SESSION_ID: process.env.SESSION_ID || "t5YmSboI#NjlaxQysKPGiRv3c2kC-ayL5IXuCvfyhe2Bzc6FJjCc",
 };
